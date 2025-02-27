2a4f88274ee252d5d12bf67d53c26d4f7d155c04
vasilij korobov <kavyabass51@gmail.com>
2025-02-27 21:11:42 +0700

    Задание 4. Реализация контроллеров

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
 ]
