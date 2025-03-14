00041a15b580e826ac07d688de9c5e951a5684bd
vasilij korobov <kavyabass51@gmail.com>
2025-03-14 22:44:33 +0700

    Задание 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
@@ -13,3 +14,11 @@ def contacts(request):
 
         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'catalog/product_detail.html', context=context)
