8c003044b5c5cf5e3a1fdbd04e150c78618ff239
vasilij korobov <kavyabass51@gmail.com>
2025-03-15 16:43:15 +0700

    Задание 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
-
+from .views import HomeView, ContactsView, ProductDetailView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    # path('home/', views.home, name='home'),
+    # path('contacts/', views.contacts, name='contacts'),
+    # path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+
+    path('home/', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,28 +1,54 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {
-        'products': products,
-    }
-    return render(request, 'catalog/home.html', context=context)
+# def home(request):
+#     products = Product.objects.all()
+#     context = {
+#         'products': products,
+#     }
+#     return render(request, 'catalog/home.html', context=context)
+#
+#
+# def contacts(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         message = request.POST.get('message')
+#
+#         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
+#     return render(request, 'catalog/contacts.html')
+#
+#
+# def product_detail(request, product_id):
+#     product = Product.objects.get(id=product_id)
+#     context = {
+#         'product': product,
+#     }
+#     return render(request, 'catalog/product_detail.html', context=context)
+
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
 
-def contacts(request):
-    if request.method == 'POST':
+class ContactsView(View):
+    template_name = 'catalog/contacts.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
         name = request.POST.get('name')
         message = request.POST.get('message')
-
         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
-    return render(request, 'catalog/contacts.html')
 
 
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {
-        'product': product,
-    }
-    return render(request, 'catalog/product_detail.html', context=context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'product_id'
