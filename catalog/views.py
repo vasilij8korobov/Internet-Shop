dc6848c9b316347bc7f0a78276c92fb1129592d2
vasilij8korobov <kavyabass51@gmail.com>
2025-03-16 23:13:18 +0700

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -21,7 +21,7 @@ def contacts(request):
 
 
 def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product, pk=product_id)
     context = {
         'product': product,
     }
