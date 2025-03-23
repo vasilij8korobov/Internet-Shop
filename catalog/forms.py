ba090373460222adf68cf72f5db10554ec1f5586
vasilij korobov <kavyabass51@gmail.com>
2025-03-23 09:51:32 +0700

    доделки

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
+
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    price = forms.IntegerField(validators=[MinValueValidator(0)])
+
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
@@ -21,24 +26,25 @@ class ProductForm(forms.ModelForm):
         description = self.cleaned_data.get('description')
         return self.check_forbidden_words(description)
 
-    def clean_price(self):
-        price = self.cleaned_data['price']
-        if price is not None and price < 0:
-            raise forms.ValidationError("Цена не может быть отрицательной или отсутствовать.")
-        return price
+    # def clean_price(self):
+    #     price = self.cleaned_data['price']
+    #     if price is not None and price < 0:
+    #         raise ValidationError("Цена не может быть отрицательной или отсутствовать.")
+    #     return price
 
     def clean_image(self):
         image = self.cleaned_data.get('image')
         if image:
             if image.size > 5 * 1024 * 1024:  # 5 MB
-                raise forms.ValidationError("Размер изображения не может превышать 5 МБ.")
+                raise ValidationError("Размер изображения не может превышать 5 МБ.")
             if not image.content_type in ['image/jpeg', 'image/png']:
-                raise forms.ValidationError("Допустимые форматы изображения: JPEG, PNG.")
+                raise ValidationError("Допустимые форматы изображения: JPEG, PNG.")
         return image
 
     def check_forbidden_words(self, value):
-        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
         for word in forbidden_words:
             if word in value.lower():
-                raise forms.ValidationError(f"Слово '{word}' запрещено к использованию.")
+                raise ValidationError(f"Слово '{word}' запрещено к использованию.")
         return value
