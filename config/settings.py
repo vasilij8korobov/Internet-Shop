4ba665f37d1417c604b61c05040ab8f6f2879d0b
vasilij korobov <kavyabass51@gmail.com>
2025-04-03 15:49:59 +0700

    Задача 2

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'Vasya228korobov@yandex.ru'
+EMAIL_HOST_PASSWORD = 'qposabxwcloffjzv'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
